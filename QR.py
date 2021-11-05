6487d7635bc067a0f3d06a57e776dd3507c1aebb
CrystalEze <90790316+CrystalEze@users.noreply.github.com>
2021-11-04 23:03:58 -0500

    Add files via upload
    Deleted Unstable Gram Schmidt code, added new function

--- QR.py
+++ QR.py
@@ -2,47 +2,6 @@ import LA
 
 #1
 
-def Unstable_GS(matrix_a: list[list[float]]) -> list[list[float]]: 
-    """Implements the unstable version of a Gram-Schmidt reduced QR factorization
-
-    First we start by setting our three variables. Q is an empty list, V is a matrix filled with
-    zeros and is the same size as matrix A, R is a matrix filled with zeros, and the v is also a float 
-    equivalent to 0. After we initialize the variables, we create our first for loop which will run the length
-    of matrix A. Inside that for loop, our V will be overwritten by the same Matrix A column. Inside that we'll 
-    run another for loop that goes from  0 to the column. It will first override our R row and columns, or our
-    inner and outer index, with our LA dot product function with our variables Q and V which represents the 
-    corresponding row and column. 
-    Next we will override the v with our LA scalar vector multiplication function with the variables
-    Q and -R, Q representing our row and -R representing both column and row. We will then override our V[column]
-    with our LA vector addition function with the variables V[column] and the float v. Our R columns will 
-    then be overwritten by our LA p-norm function, which will finally be appended to our Q which is the LA 
-    scalar vector multiplication function with the variables V[column] and 1 divided by the diagonal inputs of R.
-
-    Args:
-        matrix_a: A matrix which is the result of our Q and R, stored as a list of lists
-    
-    Returns:
-        A list of two matrices, the first being Q, and the second being R
-    """
-
-    Q: list[complex or float] = []
-    V: list[complex or float] = [[0,0,0], [0,0,0]] 
-    R: list[complex or float] = [[0,0,0], [0,0,0]]
-    v = 0
-
-    for column in range(len(matrix_a)):
-        V[column] = matrix_a[column]
-        for rows in range(0, column):
-            R[rows][column] = LA.dot_product(Q[rows], V[column])
-            v = LA.sca_vec_mult(Q[rows], -R[rows][column])
-            V[column] = LA.add_vectors(V[column], v)
-        R[column][column] = LA.P_norm((V[column]))
-        Q.append(LA.sca_vec_mult((V[column]), (1/R[column][column])))
-    return [Q,R]
-
-
-#2
-
 def Stable_GS(matrix_a: list[list[float]]) -> list[list[float]]:
     """Implements the stable version of a Gram-Schmidt reduced QR factorization
 
@@ -83,3 +42,24 @@ def Stable_GS(matrix_a: list[list[float]]) -> list[list[float]]:
             v = LA.sca_vec_mult(Q[column], -R[column][rows])
             V[rows] = LA.add_vectors(V[rows], v)
     return [Q,R]
+
+
+#2
+
+def Orthonormal_Vectors(matrix_a: list[list[float]]) -> list[list[float]]:
+    """Creates a list of orthonormal vectors
+
+    First take each column in your matrix and set it equal to any variable of your choosing. This
+    represents one of many of your input vectors. To find your orthonormal vectors, you first take
+    eachh element of the vector and divide it by the magnitude of the vector. Once that's done, we can
+    repeat that step for the rest of the vectors in the matrix.
+
+    Args:
+        matrix_a: A matrix stored as a list of vectors
+    
+    Returns:
+        An orthonormal list of vectors
+    """
+
+    result: list[list] = Stable_GS(matrix_a)[0]
+    return result
